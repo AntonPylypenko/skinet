a4f68375c971b306d607968937fdf4d91e6e26ed
Anton Pylypenko <anton.pylypenko5@gmail.com>
2022-02-08 00:16:39 +0200

    sortin, filtering, pagination, cors are added (6)

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -6,10 +8,7 @@ namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
-        {
-        }
-
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options){}
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
@@ -18,6 +17,25 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //так як SqlLite не може мати типу decimal ми перевіряємо по всім полям, 
+            //де є децімал - перетворюємо їх в дабл
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType
+                    .GetProperties()
+                    .Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name)
+                        .Property(property.Name)
+                        .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
